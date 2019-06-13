fa2f8f8ff24cc5f2f1d046d8c53b11571eabc2c0
jstray <jonathanstray@gmail.com>
2019-06-13 09:51:00 -0700

    Directory structure cleanups

---
+++ source/README.md
@@ -0,0 +1,4 @@
+ftf-all-filings.tsv is the crowdsourced data entered by volunteers in 2012.
+The dc-slug is can be used to get a URL for the original PDF; see download-pdfs.py
+
+Originally from https://www.propublica.org/datastore/dataset/free-the-files-filing-data
--- train.py
+++ train.py
@@ -1,48 +1,46 @@
-from numpy import array
 import keras as K
-from keras.preprocessing.text import one_hot
-from keras.preprocessing.sequence import pad_sequences
 from keras.engine.input_layer import Input
-from keras.layers import Dense, Flatten, Conv1D, MaxPooling1D, Lambda, Conv2DTranspose, concatenate
-from keras.layers.embeddings import Embedding
 from keras.models import Model
+from keras.layers import Dense, Flatten, Dropout, Lambda, concatenate
+from keras.layers.embeddings import Embedding
 from keras.backend import expand_dims, squeeze
 import tensorflow as tf
 import pandas as pd
 import numpy as np
 import csv
 import re
+import random
 import pdfplumber
+import os
+import pickle
 from decimal import Decimal
 
 import wandb
 from wandb.keras import WandbCallback
 
+# Configuration
 run = wandb.init()
 config = run.config
-config.epochs = 25
+config.read_docs = 10000 # how many docs to load, at most
+config.window_len = 30 # size of token sequences to train on (and network size!)
+config.vocab_size = 500
+config.token_dims = 7 # number of features per token, including token hash
+config.positive_fraction = 0.5
+config.target_thresh = 0.9 # target match scores larger than this will becomes positive labels
+config.epochs = 50
+config.batch_size=10000
+config.steps_per_epoch = 10
+config.doc_acc_sample_size = 25 # how many documents to check extraction on after each epoch
+config.penalize_missed = 10 # much more more a missed 1 counts than a missed 0 in output
+config.val_split = 0.2
 
-# Thanks, StackOverflow. This "undoes" a 1D convolution, by combining upsampling plus convolution.
-def Conv1DTranspose(input_tensor, filters, kernel_size, strides=2, padding='same'):
-		x = Lambda(lambda x: expand_dims(x, axis=2))(input_tensor)
-		x = Conv2DTranspose(filters=filters, kernel_size=(kernel_size, 1), strides=(strides, 1), padding=padding)(x)
-		x = Lambda(lambda x: squeeze(x, axis=2))(x)
-		return x
+# ---- Load data and generate features ----
 
-# Configuration
-read_docs = 10000 # how many docs to load, at most
-max_doc_length = 4096
-vocab_size = 5000
-target_thresh = 0.9
-augment_dims = 4 # number of features per token, other then token type
-
-wandb.log({'algorithm':'U-net with position and dollar marker'})
-
-# Generator that reads all our training data
+# Generator that reads raw training data
 # For each document, yields an array of dictionaries, each of which is a token
 def input_docs(max_docs=None):
 	incsv = csv.DictReader(open('data/training.csv', mode='r'))
-    
+		
 	# Reconstruct documents by concatenating all rows with the same slug
 	active_slug = None
 	doc_rows = [] 
@@ -68,169 +66,248 @@ def input_docs(max_docs=None):
 	yield doc_rows
 
 
-# --- additional features ----
 def is_dollar_amount(s):
 	return re.search(r'\$?\d[\d,]+(\.\d\d)?',s) != None
 
-def augment_row(row):
-	return [ float(row['page']), 
+def token_features(row, vocab_size):
+	tokstr = row['token'].upper()
+	return [ hash(tokstr) % vocab_size,
+					 float(row['page']), 
 					 float(row['x0']),
 					 float(row['y0']), 
-					 float(is_dollar_amount(row['token'])) ]
+					 float(len(tokstr)),
+					 float(np.mean([c.isdigit() for c in tokstr])),
+					 float(is_dollar_amount(tokstr)) ]
 	
-def pad_list(x,maxlen,padv):
-	n = len(x)
-	if n>maxlen:
-		return x[:maxlen]
-	elif n<maxlen:
-		return x + [padv]*(maxlen-n)
+# Load raw training data, create our per-token features and binary labels	
+def load_training_data_nocache(config):
+	slugs = []
+	token_text = []
+	features = []
+	labels = []
+	for doc_tokens in input_docs(max_docs=config.read_docs):	
+		if len(doc_tokens) < config.window_len:
+			continue # TODO pad shorter docs
+			
+		# Not training data, but used for evaluating results later
+		slugs.append(doc_tokens[0]['slug']) # unique document ID, also PDF filename
+		token_text.append([row['token'] for row in doc_tokens])
+
+		features.append([token_features(row, config.vocab_size) for row in doc_tokens])
+		
+		# threshold fuzzy matching score with our target field, to get binary labels 
+		labels.append([(0 if float(row['gross_amount']) < config.target_thresh else 1) for row in doc_tokens])
+
+	return slugs, token_text, features, labels
+	
+# Because generating the list of features is so expensive, we cache it on disk
+def load_training_data(config):
+	if os.path.isfile('data/cached_features.p'):
+		print('Loading training data from cache...')
+		slugs, token_text, features,labels = pickle.load(open('data/cached_features.p', 'rb'))
 	else:
-		return x
+		print('Loading training data...')
+		slugs, token_text, features, labels = load_training_data_nocache(config)
+		print('Saving training data to cache...')
+		pickle.dump((slugs, token_text, features, labels), open('data/cached_features.p', 'wb'))
 
+	return slugs, token_text, features,labels
 
-# --- Create training data ---
-print('Loading training data...')
-docs = []
-labels = []
-augment = []
-for docrows in input_docs(max_docs=read_docs):	
-	# reconstruct document text (will be tokenized again below, huh)
-	docs.append(' '.join([row['token'] for row in docrows]))
 	
-	# threshold fuzzy matching score with our target field, to get binary labels 
-	labels.append([(0 if float(row['gross_amount']) < target_thresh else 1) for row in docrows])
-	
-	augment.append(	pad_list([augment_row(row) for row in docrows], 
-									max_doc_length, 
-									[0]*augment_dims))
+# ---- Resample features,labels as windows ----
 
-print(f'Loaded {len(docs)}')
+# returns a window of tokens, labels at a random position in a random document
+def one_window_unbalanced(features, labels, window_len):
+	doc_idx = random.randint(0,len(features)-1)
+	doc_len = len(features[doc_idx])
+	tok_idx = random.randint(0, doc_len-window_len)
+	return features[doc_idx][tok_idx : tok_idx+window_len], labels[doc_idx][tok_idx : tok_idx+window_len]
+
+# control the fraction of windows that include a positive label. not efficient.
+def one_window(features, labels, window_len, positive_fraction):
+	f,l = one_window_unbalanced(features, labels, window_len)
+	if random.random() > positive_fraction: # mostly positive examples
+		while not 1 in l:
+			f,l = one_window_unbalanced(features, labels, window_len)
+	return f,l
+
+def windowed_generator(features, labels, config):
+	# Create empty arrays to contain batch of features and labels#
+	batch_features = np.zeros((config.batch_size, config.window_len, config.token_dims))
+	batch_labels = np.zeros((config.batch_size, config.window_len))
+
+	while True:
+		for i in range(config.batch_size):
+			features1,labels1 = one_window(features, labels, config.window_len, config.positive_fraction)
+			batch_features[i,:,:] = features1
+			batch_labels[i,:] = labels1
+		yield batch_features, batch_labels
+
+# ---- Custom loss function is basically MSE but high penalty for missing a 1 label ---
+
+def missed_token_loss(one_penalty):
+   
+	def _missed_token_loss(y_true, y_pred):
+	  expected_zero = tf.to_float(tf.math.equal(y_true,0))
+	  s = y_pred*expected_zero
+	  zero_loss = K.backend.mean(K.backend.square(s))
+	  expected_one = tf.to_float(tf.math.equal(y_true,1))
+	  t = one_penalty*(1-y_pred)*expected_one
+	  one_loss = K.backend.mean(K.backend.square(t))
+	  return zero_loss + one_loss
+
+	return _missed_token_loss # closes over one_penalty
+
+# --- Specify network ---
+
+def create_model(config):
+	indata = Input((config.window_len, config.token_dims))
+
+	# split into the hash and the rest of the token features, embed hash as one-hot, then merge
+	tok_hash = Lambda( lambda x: squeeze(K.backend.slice(x, (0,0,0), (-1,-1,1)),axis=2))(indata)
+	tok_features = Lambda( lambda x: K.backend.slice(x, (0,0,1), (-1,-1,-1)))(indata)
+	embed = Embedding(config.vocab_size, 32)(tok_hash)
+	merged = concatenate([embed, tok_features], axis=2)
+
+	f = Flatten()(merged)
+	d1 = Dense(config.window_len*config.token_dims*5, activation='sigmoid')(f)
+	d2 = Dropout(0.3)(d1)
+	d3 = Dense(config.window_len*config.token_dims, activation='sigmoid')(d2)
+	d4 = Dropout(0.3)(d3)
+	d5 = Dense(config.window_len, activation='elu')(d4)
+
+	model = Model(inputs=[indata], outputs=[d5])
+	model.compile(
+		optimizer='adam', 
+		loss=missed_token_loss(config.penalize_missed), 
+		metrics=['acc'])
+
+	return model
+
+# --- Predict ---
+# Our network is windowed, so we have to aggregate windows to get a final score
+
+# Returns vector of token scores
+def predict_scores(model, features, window_len):
+	doc_len = len(features)
+	num_windows = doc_len-window_len
+
+	windowed_features = np.array([features[i:i+window_len] for i in range(num_windows)])
+	window_scores = model.predict(windowed_features)
+
+	scores = np.zeros(doc_len)
+	for i in range(num_windows):
+		scores[i:i+window_len] += window_scores[i] # would max work better than sum?
+	return scores
+
+# returns text, score of best answer
+def predict_answer(model, features, token_text, window_len):
+	scores = predict_scores(model, features, window_len)
+	best_score_idx = np.argmax(scores)
+	best_score_text = token_text[best_score_idx]
+	return best_score_text, scores[best_score_idx]
+
+# returns text of correct answer,
+def correct_answer(features, labels, token_text):
+	answer_idx = np.argmax(labels)
+	answer_text = token_text[answer_idx]
+	return answer_text
+
+# Calculate accuracy of answer extraction over num_to_test docs, print diagnostics while we do so
+def compute_accuracy(model, window_len, slugs, token_text, features, labels, num_to_test):
+	acc = 0.0
+	for i in range(num_to_test):
+		doc_idx = random.randint(0, len(slugs)-1)
+		predict_text, predict_score = predict_answer(model, features[doc_idx], token_text[doc_idx], window_len)
+		answer_text = correct_answer(features[doc_idx], labels[doc_idx], token_text[doc_idx])
+		print(f'{slugs[doc_idx]}: guessed "{predict_text}" with score {predict_score}, correct "{answer_text}"')
+		if predict_text==answer_text:
+			acc+=1
+	return acc/num_to_test
+
+
+# ---- Custom callback to log document-level accuracy ----
+
+class DocAccCallback(K.callbacks.Callback):
+	def __init__(self, window_len, slugs, token_text, features, labels, num_to_test, logname):
+		self.window_len = window_len
+		self.slugs = slugs
+		self.token_text = token_text
+		self.features = features
+		self.labels = labels
+		self.num_to_test = num_to_test
+		self.logname = logname
+
+	def on_epoch_end(self, epoch, logs):
+		acc = compute_accuracy(self.model,
+			self.window_len,
+			self.slugs,
+			self.token_text,
+			self.features,
+			self.labels,
+			self.num_to_test+epoch)  # test more docs later in training, for more precise acc
+		print(f'This epoch {self.logname}: {acc}')
+		wandb.log({self.logname:acc})
+
+
+# --- MAIN ----
+
+print('Configuration:')
+print(config)
+
+slugs, token_text, features, labels = load_training_data(config)
+print(f'Loaded {len(features)}')
 max_length = max([len(x) for x in labels])
 print(f'Max document size {max_length}')
 avg_length = sum([len(x) for x in labels])/len(labels)
 print(f'Average document size {avg_length}')
 
-# integer encode the documents, truncate to max_doc_length
-encoded_docs = [one_hot(d, vocab_size) for d in docs]
-x = pad_sequences(encoded_docs, maxlen=max_doc_length, dtype=np.float32, padding='post', truncating='post')
-x = np.expand_dims(x, axis=2)
-a = np.array(augment, dtype=np.float32)
-x = np.concatenate([x, a], axis=2)
+# split into train and test
+slugs_train = []
+token_text_train = []
+features_train = []
+labels_train = []
+slugs_val = []
+token_text_val = []
+features_val = []
+labels_val = []
+for i in range(len(features)):
+	if random.random() < config.val_split:
+		slugs_val.append(slugs[i])
+		token_text_val.append(token_text[i])
+		features_val.append(features[i])
+		labels_val.append(labels[i])
+	else:
+		slugs_train.append(slugs[i])
+		token_text_train.append(token_text[i])		
+		features_train.append(features[i])
+		labels_train.append(labels[i])
 
-# Truncate to max_doc_length
-y = pad_sequences(labels, maxlen=max_doc_length, padding='post', truncating='post')
+print(f'Training on {len(features_train)}, validating on {len(features_val)}')
 
-# --- Specify network ---
-
-# We use a U-net to handle long range dependencies between tokens 
-indata = Input((max_doc_length, augment_dims+1))
-tok_word = Lambda( lambda x: squeeze(K.backend.slice(x, (0,0,0), (-1,-1,1)),axis=2))(indata)
-tok_feature = Lambda( lambda x: K.backend.slice(x, (0,0,1), (-1,-1,-1)))(indata)
-embed = Embedding(vocab_size, 32)(tok_word)
-embed = concatenate([embed, tok_feature], axis=2)
-
-c1 = Conv1D(filters=8, kernel_size=5, padding='same')(embed)  # 4096
-p1 = MaxPooling1D()(c1)
-c2 = Conv1D(filters=16, kernel_size=5, padding='same')(p1) # 2048
-p2 = MaxPooling1D()(c2)
-c3 = Conv1D(filters=32, kernel_size=5, padding='same')(p2) # 1024
-p3 = MaxPooling1D()(c3)
-c4 = Conv1D(filters=64, kernel_size=5, padding='same')(p3) # 512
-p4 = MaxPooling1D()(c4) # 256
-
-c5 = Conv1D(filters=64, kernel_size=5, padding='same')(p4) # 256
-
-c6 = Conv1DTranspose(c5, filters=64, kernel_size=5, padding='same') # 512
-u6 = concatenate([c4,c6], axis=2) # 512 x 128
-
-c7 = Conv1DTranspose(u6, filters=32, kernel_size=5, padding='same') # 1024
-u7 = concatenate([c3,c7], axis=2) # 1024 x 64
-
-c8 = Conv1DTranspose(u7, filters=16, kernel_size=5, padding='same') # 2048
-u8 = concatenate([c2,c8], axis=2) # 2048 x 32
-
-c9 = Conv1DTranspose(u8, filters=8, kernel_size=5, padding='same') # 4096
-u9 = concatenate([c1,c9], axis=2) # 4096 x 16
-
-# This last convolution produces the target token scores
-c10 = Conv1D(filters=1, kernel_size=10, padding='same', activation='relu')(c9)  # 4096 x 1
-f = Flatten()(c10)
-
-model = Model(inputs=[indata], outputs=[f])
-
-model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
+model = create_model(config)
 print(model.summary())
 
 
-# --- Go! ----
+model.fit_generator(
+	windowed_generator(features_train, labels_train, config),
+	steps_per_epoch=config.steps_per_epoch,
+	epochs=config.epochs,
+	callbacks=[ 
+		WandbCallback(),
+		DocAccCallback(	config.window_len, 
+										slugs_train, 
+										token_text_train, 
+										features_train, 
+										labels_train, 
+										config.doc_acc_sample_size,
+										'doc_train_acc'),
+		DocAccCallback(	config.window_len, 
+										slugs_val, 
+										token_text_val, 
+										features_val, 
+										labels_val, 
+										config.doc_acc_sample_size,
+										'doc_val_acc')
+	])
 
-model.fit(
-		x=x,
-		y=y,
-		epochs=10,
-		validation_split=0.2,
-		callbacks=[WandbCallback()])
-
-# --- Log output PDF images ---
-
-# convert a single row of document data (one token) to bbox format needed for drawing
-def docrow_to_bbox(t):
-	return [Decimal(t['x0']), Decimal(t['y0']), Decimal(t['x1']), Decimal(t['y1'])] 
-	
-cnt=0
-for doc_idx,doc_rows in enumerate(input_docs(max_docs=read_docs)):
-	slug = doc_rows[0]['slug']
-	doc_rows = doc_rows[:max_doc_length]
-	fname = 'pdfs/' + slug + '.pdf'
-	try:
-		pdf = pdfplumber.open(fname)
-	except Exception as e:
-		# If the file's not there, that's fine -- we use available PDFs to define what to see
-		continue
-
-	print('Rendering output for ' +  fname)
-
-	# Get the correct answers: find the indices of the token(s) labelled 1
-	target_idx = [idx for (idx,val) in enumerate(y[doc_idx]) if val==1]
-			
-	z = np.array([x[doc_idx]])
-	predict = model.predict(z)
-	predict = predict.squeeze(axis=0)
-	
-	# print our best guess for each dcoument
-	answer_idx = np.argmax(y[doc_idx])
-	print(f"Correct answer: {doc_rows[answer_idx]['token']} with score {y[doc_idx][answer_idx]}")
-	output_idx = np.argmax(predict)
-	print(f"Best output: {doc_rows[output_idx]['token']} with score {predict[output_idx]}")
-		
-	# Draw the machine output: get a score for each token
-	page_images=[]
-	for pagenum,page in enumerate(pdf.pages):
-		im = page.to_image(resolution=300)
-		num_pages = len(pdf.pages)>0
-		if num_pages>1:
-			current_page = pagenum/float(len(pdf.pages)-1) # training data has 0..1 for page range
-		else:
-			current_page = 0.0
-		current_page = str(current_page)
-		
-		# Draw target tokens
-		target_toks = [doc_rows[i] for i in target_idx if doc_rows[i]['page']==current_page]
-		rects = [ docrow_to_bbox(t) for t in target_toks]
-		im.draw_rects(rects, stroke='blue', stroke_width=3, fill=None)
-		
-		# Draw guesses
-		for idx,tok in enumerate(doc_rows):
-			if predict[idx]>0.1 and tok['page']==current_page:
-				c = int(255*predict[idx])
-				im.draw_rect(docrow_to_bbox(tok), 
-							 stroke=(255, 255-c, 255-c),
-							 fill=None)
-				
-		page_images.append(wandb.Image(im.annotated, caption='page ' +  str(pagenum)))
-		
-	wandb.log({slug: page_images})
-		
-	cnt+=1
-	if cnt==10:
-		break
