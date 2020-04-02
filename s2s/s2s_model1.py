5bc89031c633ae7e594ddcc2a518837ee681b665
Gray Davidson <relic@Grays-MBP-4.lan>
2020-04-01 21:52:56 -0700

    added python script to recover weights from saved model

--- s2s/s2s_model1.py
+++ s2s/s2s_model1.py
@@ -17,132 +17,141 @@ import wandb
 from wandb.keras import WandbCallback
 # add training.csv to source folder
 
-
-
-train = "../source/training.csv"
-filings = "../source/ftf-all-filings.tsv"
-docs = 200000
-truncate_length = 2000
-
-dft = pd.read_csv(train, nrows = docs)
-dft.head()
-
-dff = pd.read_csv(filings,nrows = docs, sep='\t')
-dff.head()
-
-df_all = pd.merge(left=dft, right=dff, how='left', left_on='slug', right_on='dc_slug')
-#df_all = df_all[['slug', 'page', 'x0', 'y0', 'x1', 'y1', 'token', 'gross_amount_x', 'committee']]
-df_all = df_all[['slug', 'token', 'committee']]
-
-df_group = df_all.groupby(['slug','committee'])['token'].apply(lambda a: ' '.join([str(x) for x in a])).reset_index()
-print(df_group.shape)
-print ('number of documents')
-
-df_group['text'] = df_group['token'].str.slice(0,truncate_length)
-df_group['committee'] = '\t'+ df_group['committee'] +'\n'
-
-df_group.drop(['token'], axis = 1)
-
-
-print(df_group['committee'][:3])
-
-batch_size = 64  # Batch size for training.
-epochs = 3  # Number of epochs to train for.
+batch_size = 100  # Batch size for training.
+epochs = 50  # Number of epochs to train for.
 latent_dim = 256  # Latent dimensionality of the encoding space.
-num_samples = 1000  # Number of samples to train on.
+num_samples = 4000  # Number of samples to train on.
 
-# Path to the data txt file on disk.
-# data_path = 'fra-eng/fra.txt'
+def BuildInputTensor ():
 
-input_texts = []
-input_texts = df_group['text'][:num_samples]
+	train = "../source/training.csv"
+	filings = "../source/ftf-all-filings.tsv"
 
-input_characters = set()
-for token in input_texts:
-        for char in token:
-            if char not in input_characters:
-                input_characters.add(char)
-            if ' ' not in input_characters: 
-                input_characters.add(' ')
-        if '\t' not in input_characters: 
-            input_characters.add('\t')   
+	#docs = 200000
+	truncate_length = 3000
 
+	dft = pd.read_csv(train)#, nrows = docs)
+	dft.head()
 
-target_texts = []
-target_texts = df_group['committee'][:num_samples]
+	dff = pd.read_csv(filings, sep='\t')
+	dff.head()
 
-target_characters = set()
-for token in target_texts:
-    if token != 'nan':
-        for char in token:
-            if char not in target_characters:
-                target_characters.add(char)
-        if ' ' not in target_characters: 
-            target_characters.add(' ')
-        if '\t' not in target_characters: 
-            target_characters.add('\t')        
-        
-input_characters = list(input_characters)
-target_characters = list(target_characters)
+	df_all = pd.merge(left=dft, right=dff, how='left', left_on='slug', right_on='dc_slug')
+	#df_all = df_all[['slug', 'page', 'x0', 'y0', 'x1', 'y1', 'token', 'gross_amount_x', 'committee']]
+	df_all = df_all[['slug', 'token', 'committee']]
 
-#---------------------------------
-num_encoder_tokens = len(input_characters)
-num_decoder_tokens = len(target_characters)
-max_encoder_seq_length = max([len(txt) for txt in input_texts])
-max_decoder_seq_length = max([len(txt) for txt in target_texts])
+	df_group = df_all.groupby(['slug','committee'])['token'].apply(lambda a: ' '.join([str(x) for x in a])).reset_index()
+	print(df_group.shape)
+	print ('number of documents')
+
+	df_group['text'] = df_group['token'].str.slice(0,truncate_length)
+	df_group['committee'] = '\t'+ df_group['committee'] +'\n'
+
+	df_group.drop(['token'], axis = 1)
 
 
-
-#print('Number of samples:', len(input_texts))
-print('Number of unique input chars:', num_encoder_tokens)
-print('Number of unique output cahrs:', num_decoder_tokens)
-print('Max sequence length for inputs:', max_encoder_seq_length)
-print('Max sequence length for outputs:', max_decoder_seq_length)
-
-#print(input_texts[:10])
-#print(input_characters)
-
-wandb.init(project="seq2seq_lstm_char_test", entity="deepform", name="test1", config = {"model_type" : "lstm_seq2seq_char_test", "batch_size" : 50, "vocab_size": num_encoder_tokens})
+	print(df_group['committee'][:3])
 
 
-input_token_index = dict(
-    [(char, i) for i, char in enumerate(input_characters)])
-print (input_token_index)
-target_token_index = dict(
-    [(char, i) for i, char in enumerate(target_characters)])
+	# Path to the data txt file on disk.
+	# data_path = 'fra-eng/fra.txt'
 
-encoder_input_data = np.zeros(
-    (len(input_texts), max_encoder_seq_length, num_encoder_tokens),
-    dtype='float32')
-decoder_input_data = np.zeros(
-    (len(input_texts), max_decoder_seq_length, num_decoder_tokens),
-    dtype='float32')
-decoder_target_data = np.zeros(
-    (len(input_texts), max_decoder_seq_length, num_decoder_tokens),
-    dtype='float32')
+	input_texts = []
+	input_texts = df_group['text'][:num_samples]
 
-# print('length of input text')
-# print(len(input_text))
+	input_characters = set()
+	for token in input_texts:
+	        for char in token:
+	            if char not in input_characters:
+	                input_characters.add(char)
+	            if ' ' not in input_characters: 
+	                input_characters.add(' ')
+	        if '\t' not in input_characters: 
+	            input_characters.add('\t')   
 
-# print('length of target text')
-# print(len(target_text))
 
-for i, (input_text, target_text) in enumerate(zip(input_texts, target_texts)):
-    for t, char in enumerate(input_text):
-        encoder_input_data[i, t, input_token_index[char]] = 1.
-    encoder_input_data[i, t + 1:, input_token_index[' ']] = 1.
-    for t, char in enumerate(target_text):
-        #decoder_target_data is ahead of decoder_input_data by one timestep
-        decoder_input_data[i, t, target_token_index[char]] = 1.
-        if t > 0:
-            # decoder_target_data will be ahead by one timestep
-            # and will not include the start character.
-            decoder_target_data[i, t - 1, target_token_index[char]] = 1.
-    decoder_input_data[i, t + 1:, target_token_index[' ']] = 1.
-    decoder_target_data[i, t:, target_token_index[' ']] = 1.
+	target_texts = []
+	target_texts = df_group['committee'][:num_samples]
+
+	target_characters = set()
+	for token in target_texts:
+	    if token != 'nan':
+	        for char in token:
+	            if char not in target_characters:
+	                target_characters.add(char)
+	        if ' ' not in target_characters: 
+	            target_characters.add(' ')
+	        if '\t' not in target_characters: 
+	            target_characters.add('\t')        
+	        
+	input_characters = list(input_characters)
+	target_characters = list(target_characters)
+
+	#---------------------------------
+	num_encoder_tokens = len(input_characters)
+	num_decoder_tokens = len(target_characters)
+	max_encoder_seq_length = max([len(txt) for txt in input_texts])
+	max_decoder_seq_length = max([len(txt) for txt in target_texts])
+
+
+
+	#print('Number of samples:', len(input_texts))
+	print('Number of unique input chars:', num_encoder_tokens)
+	print('Number of unique output cahrs:', num_decoder_tokens)
+	print('Max sequence length for inputs:', max_encoder_seq_length)
+	print('Max sequence length for outputs:', max_decoder_seq_length)
+
+	#print(input_texts[:10])
+	#print(input_characters)
+
+	wandb.init(project="seq2seq_lstm_char_test", entity="deepform", name="test1", config = {"model_type" : "lstm_seq2seq_char_test", "batch_size" : 50, "vocab_size": num_encoder_tokens})
+
+
+	input_token_index = dict(
+	    [(char, i) for i, char in enumerate(input_characters)])
+	#print (input_token_index)
+	target_token_index = dict(
+	    [(char, i) for i, char in enumerate(target_characters)])
+
+	encoder_input_data = np.zeros(
+	    (len(input_texts), max_encoder_seq_length, num_encoder_tokens),
+	    dtype='float32')
+	decoder_input_data = np.zeros(
+	    (len(input_texts), max_decoder_seq_length, num_decoder_tokens),
+	    dtype='float32')
+	decoder_target_data = np.zeros(
+	    (len(input_texts), max_decoder_seq_length, num_decoder_tokens),
+	    dtype='float32')
+
+	# print('length of input text')
+	# print(len(input_text))
+
+	# print('length of target text')
+	# print(len(target_text))
+
+	for i, (input_text, target_text) in enumerate(zip(input_texts, target_texts)):
+	    for t, char in enumerate(input_text):
+	        encoder_input_data[i, t, input_token_index[char]] = 1.
+	    encoder_input_data[i, t + 1:, input_token_index[' ']] = 1.
+	    for t, char in enumerate(target_text):
+	        #decoder_target_data is ahead of decoder_input_data by one timestep
+	        decoder_input_data[i, t, target_token_index[char]] = 1.
+	        if t > 0:
+	            # decoder_target_data will be ahead by one timestep
+	            # and will not include the start character.
+	            decoder_target_data[i, t - 1, target_token_index[char]] = 1.
+	    decoder_input_data[i, t + 1:, target_token_index[' ']] = 1.
+	    decoder_target_data[i, t:, target_token_index[' ']] = 1.
+
+    print (target_characters)
+	return encoder_input_data, num_decoder_tokens, max_decoder_seq_length, num_encoder_tokens, max_encoder_seq_length, decoder_input_data, decoder_target_data, input_token_index, target_token_index
+
+encoder_input_data, num_decoder_tokens, max_decoder_seq_length, num_encoder_tokens, max_encoder_seq_length, decoder_input_data, decoder_target_data, input_token_index, target_token_index = BuildInputTensor ()
+
 
 # Define an input sequence and process it.
 encoder_inputs = Input(shape=(None, num_encoder_tokens))
+
 encoder = LSTM(latent_dim, return_state=True)
 encoder_outputs, state_h, state_c = encoder(encoder_inputs)
 
@@ -163,7 +172,7 @@ decoder_outputs = decoder_dense(decoder_outputs)
 
 # Define the model that will turn
 # `encoder_input_data` & `decoder_input_data` into `decoder_target_data`
-model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
+model = Model([encoder_inputs, decoder_inputs], decoder_outputs, )
 
 
 
