931a3a0b291a8ec8787eaf7c93e019e65922ab28
Gray Davidson <relic@Grays-MBP-4.lan>
2020-04-04 18:35:54 -0700

    beam search almost

--- s2s/s2s_model1.py
+++ s2s/s2s_model1.py
@@ -7,7 +7,7 @@ import sys
 print (sys.executable)
 
 import pandas as pd
-
+import math
 
 from tensorflow.python.keras.models import Model
 from tensorflow.python.keras.layers import Input, LSTM, Dense
@@ -25,9 +25,9 @@ latent_dim = 256  # Latent dimensionality of the encoding space.
 num_samples = 4000  # Number of samples to train on.
 test_outputs = 20
 
-saved_model = 's2s_politics1_3_31_20.h5'
-mode = 'train'
-
+saved_model = 's2s_politics1_4_2_20_first_model_outputting_committees.h5'
+trainmode = False 
+beamsearch = True
 
 # Fixed character set for 1-hot encoding of inputs and outputs
 charset = string.printable + '\t\n\x00' # \t=start, \n=stop, \x00=unknown character
@@ -47,94 +47,96 @@ reverse_input_char_index = dict(
     (i, char) for char, i in input_token_index.items())
 reverse_target_char_index = dict(
     (i, char) for char, i in target_token_index.items())
+reverse_target_char_index[95] = '\t'
+reverse_target_char_index[96] = '\n'
 
 
 def clean_text(text):
-	def clean_char(c):
-		if c in charset:
-			return c
-		else:
-			return '\x00'
+    def clean_char(c):
+        if c in charset:
+            return c
+        else:
+            return '\x00'
 
-	return [clean_char(x) for x in text]
+    return [clean_char(x) for x in text]
 
 
 # Load training data and encode into 1-hot tensors
 def BuildInputTensor ():
 
-	train = "../source/training.csv"
-	filings = "../source/ftf-all-filings.tsv"
+    train = "../source/training.csv"
+    filings = "../source/ftf-all-filings.tsv"
 
-	#docs = 200000
-	truncate_length = 3000
+    #docs = 200000
+    truncate_length = 3000
 
-	dft = pd.read_csv(train)#, nrows = docs)
-	dff = pd.read_csv(filings, sep='\t')
+    dft = pd.read_csv(train)#, nrows = docs)
+    dff = pd.read_csv(filings, sep='\t')
 
-	df_all = pd.merge(left=dft, right=dff, how='left', left_on='slug', right_on='dc_slug')
-	#df_all = df_all[['slug', 'page', 'x0', 'y0', 'x1', 'y1', 'token', 'gross_amount_x', 'committee']]
-	df_all = df_all[['slug', 'token', 'committee']]
+    df_all = pd.merge(left=dft, right=dff, how='left', left_on='slug', right_on='dc_slug')
+    #df_all = df_all[['slug', 'page', 'x0', 'y0', 'x1', 'y1', 'token', 'gross_amount_x', 'committee']]
+    df_all = df_all[['slug', 'token', 'committee']]
 
-	df_group = df_all.groupby(['slug','committee'])['token'].apply(lambda a: ' '.join([str(x) for x in a])).reset_index()
-	print(df_group.shape)
-	print ('number of documents')
+    df_group = df_all.groupby(['slug','committee'])['token'].apply(lambda a: ' '.join([str(x) for x in a])).reset_index()
+    print(df_group.shape)
+    print ('number of documents')
 
-	df_group['text'] = df_group['token'].str.slice(0,truncate_length)
-	df_group['committee'] = '\t'+ df_group['committee'] +'\n'
+    df_group['text'] = df_group['token'].str.slice(0,truncate_length)
+    df_group['committee'] = '\t'+ df_group['committee'] +'\n'
 
-	df_group.drop(['token'], axis = 1)
-
-
-	print(df_group['committee'][:3])
+    df_group.drop(['token'], axis = 1)
 
 
-	target_texts = df_group['committee'][:num_samples]
-	input_texts = df_group['text'][:num_samples]
+    print(df_group['committee'][:3])
 
 
-	#---------------------------------
-
-	max_encoder_seq_length = max([len(txt) for txt in input_texts])
-	max_decoder_seq_length = max([len(txt) for txt in target_texts])
-
-	print('Number of unique input chars:', num_encoder_tokens)
-	print('Number of unique output cahrs:', num_decoder_tokens)
-	print('Max sequence length for inputs:', max_encoder_seq_length)
-	print('Max sequence length for outputs:', max_decoder_seq_length)
+    target_texts = df_group['committee'][:num_samples]
+    input_texts = df_group['text'][:num_samples]
 
 
-	encoder_input_data = np.zeros(
-	    (len(input_texts), max_encoder_seq_length, num_encoder_tokens),
-	    dtype='float32')
-	decoder_input_data = np.zeros(
-	    (len(input_texts), max_decoder_seq_length, num_decoder_tokens),
-	    dtype='float32')
-	decoder_target_data = np.zeros(
-	    (len(input_texts), max_decoder_seq_length, num_decoder_tokens),
-	    dtype='float32')
+    #---------------------------------
 
-	for i, (input_text, target_text) in enumerate(zip(input_texts, target_texts)):
-	    input_text = clean_text(input_text)
-	    for t, char in enumerate(input_text):
-	        encoder_input_data[i, t, input_token_index[char]] = 1.
-	    encoder_input_data[i, t + 1:, input_token_index[' ']] = 1.
-	    target_text = clean_text(target_text)
-	    for t, char in enumerate(target_text):
-	        #decoder_target_data is ahead of decoder_input_data by one timestep
-	        decoder_input_data[i, t, target_token_index[char]] = 1.
-	        if t > 0:
-	            # decoder_target_data will be ahead by one timestep
-	            # and will not include the start character.
-	            decoder_target_data[i, t - 1, target_token_index[char]] = 1.
-	    decoder_input_data[i, t + 1:, target_token_index[' ']] = 1.
-	    decoder_target_data[i, t:, target_token_index[' ']] = 1.
+    max_encoder_seq_length = max([len(txt) for txt in input_texts])
+    max_decoder_seq_length = max([len(txt) for txt in target_texts])
+
+    print('Number of unique input chars:', num_encoder_tokens)
+    print('Number of unique output cahrs:', num_decoder_tokens)
+    print('Max sequence length for inputs:', max_encoder_seq_length)
+    print('Max sequence length for outputs:', max_decoder_seq_length)
 
 
-	return encoder_input_data, max_decoder_seq_length, max_encoder_seq_length, decoder_input_data, decoder_target_data, input_texts, target_texts
+    encoder_input_data = np.zeros(
+        (len(input_texts), max_encoder_seq_length, num_encoder_tokens),
+        dtype='float32')
+    decoder_input_data = np.zeros(
+        (len(input_texts), max_decoder_seq_length, num_decoder_tokens),
+        dtype='float32')
+    decoder_target_data = np.zeros(
+        (len(input_texts), max_decoder_seq_length, num_decoder_tokens),
+        dtype='float32')
+
+    for i, (input_text, target_text) in enumerate(zip(input_texts, target_texts)):
+        input_text = clean_text(input_text)
+        for t, char in enumerate(input_text):
+            encoder_input_data[i, t, input_token_index[char]] = 1.
+        encoder_input_data[i, t + 1:, input_token_index[' ']] = 1.
+        target_text = clean_text(target_text)
+        for t, char in enumerate(target_text):
+            #decoder_target_data is ahead of decoder_input_data by one timestep
+            decoder_input_data[i, t, target_token_index[char]] = 1.
+            if t > 0:
+                # decoder_target_data will be ahead by one timestep
+                # and will not include the start character.
+                decoder_target_data[i, t - 1, target_token_index[char]] = 1.
+#       decoder_input_data[i, t + 1:, target_token_index[' ']] = 1.
+#       decoder_target_data[i, t:, target_token_index[' ']] = 1.
+
+
+    return encoder_input_data, max_decoder_seq_length, max_encoder_seq_length, decoder_input_data, decoder_target_data, input_texts, target_texts
 
 encoder_input_data, max_decoder_seq_length, max_encoder_seq_length, decoder_input_data, decoder_target_data, input_texts, target_texts = BuildInputTensor ()
 
-wandb.init(project="seq2seq_lstm_char_test", entity="deepform", name="test1", config = {"model_type" : "lstm_seq2seq_char_test", "batch_size" : 50, "vocab_size": num_encoder_tokens})
+# wandb.init(project="seq2seq_lstm_char_test", entity="deepform", name="test1", config = {"model_type" : "lstm_seq2seq_char_test", "batch_size" : 50, "vocab_size": num_encoder_tokens})
 
 
 # Define an input sequence and process it.
@@ -159,27 +161,27 @@ decoder_dense = Dense(num_decoder_tokens, activation='softmax')
 decoder_outputs = decoder_dense(decoder_outputs)
 
 
-if mode == 'train':
-	# Define the model that will turn
-	# `encoder_input_data` & `decoder_input_data` into `decoder_target_data`
-	model = Model([encoder_inputs, decoder_inputs], decoder_outputs, )
+
+    # Define the model that will turn
+    # `encoder_input_data` & `decoder_input_data` into `decoder_target_data`
+model = Model([encoder_inputs, decoder_inputs], decoder_outputs, )
 
 
+if trainmode:
+    # Run training
+    model.compile(optimizer='rmsprop', loss='categorical_crossentropy',
+                  metrics=['accuracy'])
 
-	# Run training
-	model.compile(optimizer='rmsprop', loss='categorical_crossentropy',
-	              metrics=['accuracy'])
+    model.fit([encoder_input_data, decoder_input_data], decoder_target_data,
+              batch_size=batch_size,
+              epochs=epochs,
+              validation_split=0.2, callbacks=[WandbCallback()])
 
-	model.fit([encoder_input_data, decoder_input_data], decoder_target_data,
-	          batch_size=batch_size,
-	          epochs=epochs,
-	          validation_split=0.2, callbacks=[WandbCallback()])
-
-	# Save model
-	model.save('s2s_politics1.h5')
+    # Save model
+    model.save('s2s_politics1.h5')
 
 else:
-	model.load_weights(saved_model, by_name=True)
+    model.load_weights(saved_model, by_name=True)
 
 # Next: inference mode (sampling).
 # Here's the drill:
@@ -257,16 +259,94 @@ def decode_sequence(input_seq):
 
     return decoded_sentence
 
+def decode_sequence_beam(input_seq):
+    
+    beamwidth = 3
+    live_strings = [('', 0, 0)]
+    dead_strings = []
+
+    # Encode the input as state vectors.
+    states_value = encoder_model.predict(input_seq)
+
+    # Generate empty target sequence of length 1.
+    target_seq = np.zeros((len(live_strings), 1, num_decoder_tokens))
+    # Populate the first character of target sequence with the start character.
+    target_seq[0, 0, target_token_index['\t']] = 1.
+
+    # Sampling loop for a batch of sequences
+    # (to simplify, here we assume a batch of size beamwidth).
+
+    step_counter = 0
+    
+    while len(live_strings) > 0 and step_counter < max_decoder_seq_length:
+        print(f'step {step_counter}')
+        print(f'{len(live_strings)} live strings')
+
+        output_tokens, h, c = decoder_model.predict(
+            [target_seq] + states_value)
+
+        new_live_strings = []
+
+        # Loop over live strings
+        for i in range(len(live_strings)):
+            prefix, logprob, old_index = live_strings[i]
+
+            # Loop over output chars
+            for j in range(num_decoder_tokens):
+                new_live_strings.append((prefix+reverse_target_char_index[j], logprob + math.log(1e-100 + output_tokens[i, -1, j]), i))
+
+        dead_strings.extend([l for l in new_live_strings if l[0][-1] == '\n'])        
+
+        new_live_strings = [k for k in new_live_strings if k[0][-1] != '\n']
+        new_live_strings = sorted(new_live_strings, key = lambda x: x[1], reverse = True)[:beamwidth]
+
+
+        # Update the target sequence (of length 1).
+        target_seq = np.zeros((len(new_live_strings), 1, num_decoder_tokens))
+        states_h = np.zeros((len(new_live_strings), latent_dim))
+        states_c = np.zeros((len(new_live_strings), latent_dim))
+        
+        print(f'states_h[0].shape {states_h[0].shape}')
+
+        print(f'type(h[0]) {type(h[0])}')
+        print(f'h[0].shape {h[0].shape}')
+        print(f'h[0] {h[0]}')
+
+        for i in range(len(new_live_strings)):
+            string, prob, index = new_live_strings[i]
+            target_seq[i, 0, target_token_index[string[-1]]] = 1.
+            states_h[i] = [h[index]]
+            states_c[i] = [c[index]]
+        states_value = [states_h,states_c]
+
+        # Update states
+        live_strings = new_live_strings
+        step_counter += 1
+
+    # length normalization
+    dead_strings = [(s,p/len(s)) for (s,p) in dead_strings]
+
+    return sorted(dead_strings, key = lambda x: x[1], reverse = True)
+
 
 for seq_index in range(test_outputs):
     # Take one sequence (part of the training set)
     # for trying out decoding.
     input_seq = encoder_input_data[seq_index: seq_index + 1]
-    decoded_sentence = decode_sequence(input_seq)
+
     print('-----')
     print('Input sentence:', input_texts[seq_index])
     print('Target Sentence:', target_texts[seq_index])
-    print('Decoded Sentence:', decoded_sentence)
+    if beamsearch:
+        decoded_sentences = decode_sequence_beam(input_seq)
+
+        for sentence, prob in decoded_sentences:
+            print('Decoded Sentences:')
+            print(sentence + ' ' + str(prob))
+
+    else: 
+        decoded_sentence = decode_sequence(input_seq)
+        print('Decoded Sentence:', decoded_sentence)
 
 
 
