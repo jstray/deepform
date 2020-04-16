056744063c435d847ea5a0081a1b89cf95303295
daniel <daniel.fennelly@gmail.com>
2020-04-15 23:34:33 -0700

    Modify train.py and db/source.py

--- db/source.py
+++ db/source.py
@@ -24,19 +24,20 @@ def clean_text(text):
 
 
 def input_generator(conn, num_samples=10, truncate_length=3000):
+    # TODO: pass in seed value to rand for deterministic shuffles?
     documents = pd.read_sql(
         f"select * from document where committee != '' order by rand() limit {num_samples};",
         conn
     )
     for document in documents.itertuples():
         doc_id = document.dc_slug
+        # TODO: read all tokens for a given batch of docs at a time
         tokens = pd.read_sql(
             f"select * from token where dc_slug = '{doc_id}';",
             conn
         )
-        text = ' '.join([str(token) for token in tokens['token']])
-        # yield clean_text(text), clean_text(document.committee)
-        yield text, document.committee
+        for token in tokens.itertuples():
+            yield document.dc_slug, token.token, token.x0, token.y0, token.page, document.committee, document.gross_amount_usd
 
 
 def dataset(conn, buffer_size=100):
@@ -44,7 +45,7 @@ def dataset(conn, buffer_size=100):
     ds = tf.data.Dataset.from_generator(
         gen,
         args=[],
-        output_types=(tf.string, tf.string),
+        output_types=(tf.string, tf.string, tf.double, tf.double, tf.double, tf.string, tf.double),
     )
     return ds.prefetch(buffer_size)
 
@@ -52,6 +53,6 @@ def dataset(conn, buffer_size=100):
 if __name__ == '__main__':
     conn = connection("root", "changeme")
     ds = dataset(conn)
-    for d in ds.take(10):
+    for d in ds:
         print(d)
         print("*****")
