Fix a handful of numerical issues relating to handling dollar amounts (#29)
* Fix a handful of numerical issues relating to handling dollar amounts

* Fix more issues that cropped up with dollar amounts

features.py
@@ -1,5 +1,3 @@
-import math
-
 import numpy as np
 
 import util
@@ -15,9 +13,9 @@ def token_features(row, config):
             float(row["x0"]) if c.use_geom else 0,
             float(row["y0"]) if c.use_geom else 0,
             float(len(tokstr)),
-            float(np.mean([c.isdigit() for c in tokstr])),
-            float(util.is_dollar_amount(tokstr)),
-            math.log(util.dollar_amount(tokstr) + 1) if c.use_amount else 0,
+            np.mean([c.isdigit() for c in tokstr]),
+            util.is_dollar_amount(tokstr) or 0,
+            util.log_dollar_amount(tokstr) or 0 if c.use_amount else 0,
         ]
     else:
         return [0 for i in range(config.token_dims)]